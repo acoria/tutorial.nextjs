9cd1e18f6852a4288a1fe98d7fbbf63a0d50dc7c
acoria <tewes.verena@gmail.com>
2024-04-17 16:46:59 +0200

    Make header a link

--- app/loadingPage/page.tsx
+++ app/loadingPage/page.tsx
@@ -1,5 +1,8 @@
 import { getData } from "./datasource";
 
+/*
+    While data is being fetched, the loading.tsx page is displayed. Data is heavily cached so the page will only be displayed once.
+*/
 export default async function PageWithLoadingData() {
   const data = await getData();
   return <main>{data}</main>;
