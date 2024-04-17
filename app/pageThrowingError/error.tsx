d34381ce61b4b4ec2c2f50b8a06caa933447d025
acoria <tewes.verena@gmail.com>
2024-04-17 17:30:06 +0200

    Add error page

---
+++ app/pageThrowingError/error.tsx
@@ -0,0 +1,14 @@
+/* A fallback page for thrown errors, e.g. when data fetching goes wrong.
+   The "use client" is necessary.
+   This error page is used for this route, siblings and all nested routes. It can also be added on root level to catch any error.
+*/
+"use client";
+
+export default function Error({ error }: { error: Error }) {
+  return (
+    <main>
+      <div>Oh no...An unexpected error occurred!</div>
+      <div>{`This is the error message: "${error.message}"`}</div>
+    </main>
+  );
+}
