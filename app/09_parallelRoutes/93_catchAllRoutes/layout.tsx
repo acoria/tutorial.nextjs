161475e9c0331b80dcd8a3a7bd4652c84e742825
acoria <tewes.verena@gmail.com>
2024-05-22 15:46:33 +0200

    --amend

--- app/09_parallelRoutes/93_catchAllRoutes/layout.tsx
+++ app/09_parallelRoutes/93_catchAllRoutes/layout.tsx
@@ -1,5 +1,9 @@
 import { IHaveJoinedNews } from "../types/IHaveJoinedNews";
 
+/**
+ * By catching all routes, only one page is implemented for several routes
+ * but the content can be shown depending on the different routes.
+ */
 export default function Layout({ latest, news }: IHaveJoinedNews) {
   return (
     <div>
